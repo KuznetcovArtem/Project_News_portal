904b37eac9e2e8afb6aedb2a5b335ee78d05a05a
KuznetcovArtem <toughguy1893@gmail.com>
2023-03-10 15:11:39 +0300

    D.11.5

--- project/newsapp/admin.py
+++ project/newsapp/admin.py
@@ -1,8 +1,31 @@
 from django.contrib import admin
 from .models import Author, Category, Post, PostCategory, Comment
 
-admin.site.register(Author)
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('authorUser', 'ratingAuthor')
+    list_filter = ('authorUser', 'ratingAuthor',)
+    search_fields = ('authorUser',)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('dateCreation', 'author', 'categoryType', 'rating')
+    list_filter = ('dateCreation', 'author', 'categoryType', 'rating')
+    search_fields = ('author',)
+
+
+class PostCategoryAdmin(admin.ModelAdmin):
+    list_display = ('postThrough', 'categoryThrough')
+    list_filter = ('categoryThrough',)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('commentPost', 'commentUser', 'rating')
+    list_filter = ('commentUser', 'rating')
+
+
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Category)
-admin.site.register(Post)
-admin.site.register(PostCategory)
-admin.site.register(Comment)
+admin.site.register(Post, PostAdmin)
+admin.site.register(PostCategory, PostCategoryAdmin)
+admin.site.register(Comment, CommentAdmin)
---
+++ project/newsapp/management/commands/deleteallposts.py
@@ -0,0 +1,29 @@
+from django.core.management.base import BaseCommand
+
+from newsapp.models import Category, Post
+
+
+class Command(BaseCommand):
+    help = 'python manage.py deleteallposts <имя_категории> - Команда удаляет все новости из выбранной категории.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('category', type=str)
+
+    def handle(self, *args, **options):
+        answer = input(
+            f'Вы правда хотите удалить все статьи в категории {options["category"]}? yes/no: '
+        )
+
+        if answer != 'yes':
+            self.stdout.write(self.style.ERROR('Отменено'))
+        else:
+            try:
+                category = Category.objects.get(name=options['category'])
+                Post.objects.filter(postCategory=category).delete()
+                self.stdout.write(self.style.SUCCESS(
+                    f'Все новости в категории {category.name} успешно удалены!'
+                ))
+            except category.DoesNotExist:
+                self.stdout.write(self.style.ERROR(
+                    f'Не удалось найти категорию {category.name}'
+                ))
